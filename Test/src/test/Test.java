c0bcb6e23f3ecd536afb8cf340103dd62e470648
Michael Munnik <michael.enzo.munnik@gmail.com>
2020-06-10 11:42:15 +0200

    More programs

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -9,13 +9,30 @@ package test;
  *
  * @author Enzo
  */
-public class Test {
 
+class Car {
+    String brand;
+    
+        public Car() {
+            this.brand = "Ford";
+        }
+        
+        public String getBrand() {
+            return brand;
+        }
+    
+        void run() {
+            System.out.println("Car is running...");
+        }
+    }
+public class Test {
     /**
      * @param args the command line arguments
-     */
+     */   
     public static void main(String[] args) {
-        // TODO code application logic here
+        Car ford = new Car(); 
+        String brand = ford.getBrand();
+        System.out.println(brand);
     }
     
 }
