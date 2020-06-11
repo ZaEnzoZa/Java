00ee02d740ab85637b6d87bce58a07fd957dc497
Michael Munnik <michael.enzo.munnik@gmail.com>
2020-06-11 11:18:09 +0200

    Some Classes training

---
+++ technical_training/src/technical_training/Technical_training.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package technical_training;
+
+
+
+class Person {
+    String fName;
+    int fAge;
+    
+    public Person(String sName, int sAge) {
+        fName = sName;
+        fAge = sAge;
+   }
+    
+}
+
+
+
+
+
+
+/**
+ *
+ * @author Enzo
+ */
+public class Technical_training {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Person neo = new Person("Neo", 15);
+        Person kabo = new Person("Kabo", 6);
+        System.out.println(neo.fName + " is "+ neo.fAge +" years old");
+    }
+    
+}
